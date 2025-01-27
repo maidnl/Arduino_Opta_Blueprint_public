38e5d03cc9f9e1c7554e974daea98b697330819a
maidnl <d.aimo@arduino.cc>
2025-01-27 12:21:17 +0100

    update copy firmware to copy also into the Cellular library example folder

--- fwUpdater/bin/cpFw.sh
+++ fwUpdater/bin/cpFw.sh
@@ -12,15 +12,27 @@ OAPATH="./fwUpdateAnalog.h"
 ODPATH="./fwUpdateDigital.h"
 OCPATH="./fwUpdateCellular.h"
 DST="../../examples/updateExpansionFw/"
+CPDEST="../../examples/updateCellular/"
 
 if [ -f $OAPATH ]; then
+    if [ -d $CPDEST ]; then
+        echo "Copying $OAPATH to fw updater (cellular) sketch folder ($CPDEST)"
+        mv $OAPATH $CPDEST 
+    fi
+
     echo "Moving $OAPATH to fw updater sketch folder ($DST)"
-	 mv $OAPATH $DST 
+    mv $OAPATH $DST 
 else
     echo "File $OAPATH not found"
 fi
 
 if [ -f $ODPATH ]; then
+
+    if [ -d $CPDEST ]; then
+        echo "Copying $ODPATH to fw updater (cellular) sketch folder ($CPDEST)"
+        mv $ODPATH $CPDEST 
+    fi
+
     echo "Moving $ODPATH to fw updater sketch folder ($DST)"
 	 mv $ODPATH $DST 
 else
@@ -28,6 +40,11 @@ else
 fi
 
 if [ -f $OCPATH ]; then
+    if [ -d $CPDEST ]; then
+        echo "Copying $OCPATH to fw updater (cellular) sketch folder ($CPDEST)"
+        mv $OCPATH $CPDEST
+    fi 
+
     echo "Moving $OCPATH to fw updater sketch folder ($DST)"
      mv $OCPATH $DST 
 else

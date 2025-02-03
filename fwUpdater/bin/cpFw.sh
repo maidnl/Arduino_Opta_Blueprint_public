67d4568f08566e30d05381e93293bc636618821b
maidnl <d.aimo@arduino.cc>
2025-02-03 12:26:07 +0100

    Keeping track of cellular FW version 0.1.1

--- fwUpdater/bin/cpFw.sh
+++ fwUpdater/bin/cpFw.sh
@@ -12,12 +12,12 @@ OAPATH="./fwUpdateAnalog.h"
 ODPATH="./fwUpdateDigital.h"
 OCPATH="./fwUpdateCellular.h"
 DST="../../examples/updateExpansionFw/"
-CPDEST="../../examples/updateCellular/"
+CPDEST="../../../Arduino_OptaCellular/examples/updateCellular/"
 
 if [ -f $OAPATH ]; then
     if [ -d $CPDEST ]; then
         echo "Copying $OAPATH to fw updater (cellular) sketch folder ($CPDEST)"
-        mv $OAPATH $CPDEST 
+        cp $OAPATH $CPDEST 
     fi
 
     echo "Moving $OAPATH to fw updater sketch folder ($DST)"
@@ -30,7 +30,7 @@ if [ -f $ODPATH ]; then
 
     if [ -d $CPDEST ]; then
         echo "Copying $ODPATH to fw updater (cellular) sketch folder ($CPDEST)"
-        mv $ODPATH $CPDEST 
+        cp $ODPATH $CPDEST 
     fi
 
     echo "Moving $ODPATH to fw updater sketch folder ($DST)"
@@ -42,7 +42,7 @@ fi
 if [ -f $OCPATH ]; then
     if [ -d $CPDEST ]; then
         echo "Copying $OCPATH to fw updater (cellular) sketch folder ($CPDEST)"
-        mv $OCPATH $CPDEST
+        cp $OCPATH $CPDEST
     fi 
 
     echo "Moving $OCPATH to fw updater sketch folder ($DST)"
